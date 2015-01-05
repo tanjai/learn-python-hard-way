6cb1c58f5c5385f261b13a443910796aee8ab5bb
Tanjai Kongyuen <Tanjai.Kongyuen@wotifgroup.com>
2015-01-05 16:05:06 +0700

    Practice lesson 4

--- Exercise-4-Variables-And-Names/Ex04.py
+++ Exercise-4-Variables-And-Names/Ex04.py
@@ -14,3 +14,18 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpool today."
 print "We need to put about", average_passengers_per_car, "in each car."
+
+#Study Drill
+
+Gold_Coins = 25
+Silver_Coins = 25
+Bank_notes = 100
+Total_coins = Gold_Coins + Silver_Coins 
+
+print "Study Drill from Example."
+
+print "I have got", Gold_Coins , "gold coins."
+print "I have got", Silver_Coins , "silver coins"
+print "I have got", Total_coins ,"total coins in my pocket."
+print "I have got", Bank_notes ,"bank notes."
+print "There is", Bank_notes ,"bank notes in my wallet and" , Total_coins , "coins in my pocket."
