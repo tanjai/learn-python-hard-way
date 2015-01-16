d0abd307d9d4577021fbdc5b1a5849020fab8271
Tanjai Kongyuen <tanjaik@wotifgroup.com>
2015-01-16 15:36:51 +0700

    Practice Exercise 9 and 10

---
+++ Exercise-10-What-Was-That/Ex10.py
@@ -0,0 +1,21 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+print "_____"
+
+print " Study Drills "
+
+Dog_types = " \tDog Breeds are broad categories of dogs based on function or work."
---
+++ Exercise-9-Printing-Printing-Printing/Ex09.py
@@ -0,0 +1,29 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+print " Study Drills "
+
+days = " Monday Tuesday Wednesday Thursday Friday Saturday Sunday "
+colors = "Monday = Yellow\nTuesday = Pink\nGreen = Wednesday\nThursday = Orange\nFriday = Blue\nSaturday = Purple\nSunday = Red"
+
+print "There is 7 days in a week which are : ", days
+print "Each day has own color ", colors
+
+print """
+24 Hours = 1 Day 
+7 Days = 1 week 
+4 week = 1 month 
+12 monh = 1 year 
+"""
