f6d5c5148eb4fb14fa9a16af7d055d2ccd6fc0ed
Tanjai Kongyuen <tanjaik@wotifgroup.com>
2015-01-16 12:03:27 +0700

    Practice Exercise 7

---
+++ Exercise-7-More-Printing/Ex07.py
@@ -0,0 +1,45 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10  # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+print "____"
+print " Study Drills "
+
+print "Little Red Riding Hood is a European fairy tale."
+print "It is a story about a young girl and a %s." % 'Big Bad Wolf'
+print "The story resolves around a girl called Red Riding Hood"
+print "The girl walks through the woods to deliver food to her Grand Mother"
+print "A mean wolf swallows her grandmother and waits for the girl"
+print "." * 10
+
+x1 = "When the girl arrives,"
+x2 = "The wolf jumps out of the bed"
+x3 = "and swallows her."
+x4 = "The the wolf falls asleep"
+x5 = "A lumberjack comes to the rescue."
+x6 = "A lumberjack with his axe cuts open the sleeping wolf"
+x7 = "Little Red Riding Hood and her grandmother emerge unharmed"
+x8 = "They then fill the wolf's body with heavy stones."
+x9 = "The wolf awakens and tries to flee"
+x10 = "but the stones cause him to collapse and die"
+
+print x1 + x2 + x3 + x4,
+print x5 + x6 + x7 + x8 + x9 + x10
