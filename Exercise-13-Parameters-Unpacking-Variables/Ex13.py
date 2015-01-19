a8d4ea7b85c4771f964ac09d535670f4ea7a5419
Tanjai Kongyuen <tanjaik@wotifgroup.com>
2015-01-19 16:53:31 +0700

    Edit Exercise 13

--- Exercise-13-Parameters-Unpacking-Variables/Ex13.py
+++ Exercise-13-Parameters-Unpacking-Variables/Ex13.py
@@ -12,29 +12,19 @@ print "Your second variable is:", second
 print "Your third  variable is:", third
 
 print "_____________________________________________"
+
 print " Study Drills "
 
 print "My name is        :", name         
 print "My job is         :", job          
 print "My Company Name is:", company_name 
 
-print "_____________________________________________"
+name         = raw_input("What is your name? ")
+job          = raw_input("What is your job? ")
+company_name = raw_input("What is your company name? ")
 
-print "What is your name?         "
-name         = raw_input()
-print "What is your job?          "
-job          = raw_input()
-print "What is your company name? "
-company_name = raw_input()
-
-print "Hello, I am %r and I am working as a %r at %r." % (
+print "Hello My name is %r , and I am working as a %r at %r company." % (
 	name, job, company_name)
 
 print "_____________________________________________"
 
-name         = raw_input("What is you name?")
-job          = raw_input("What is your job?")
-company_name = raw_input("What is your company name?")
-
-print "Hello, I am %r and I am working as a %r at %r" % (
-	name, job, company_name)
