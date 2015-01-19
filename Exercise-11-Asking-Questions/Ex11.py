23086399b7721fe4ea7741c513d51d62c9856e47
Tanjai Kongyuen <tanjaik@wotifgroup.com>
2015-01-19 15:12:19 +0700

    Practice Exercise 11

--- Exercise-11-Asking-Questions/Ex11.py
+++ Exercise-11-Asking-Questions/Ex11.py
@@ -1,1 +1,25 @@
 print "Exercise 11 : Asking Question"
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+print "______________"
+
+print " Study Drills "
+
+print " What is your name ?",
+name = raw_input()
+print " What is your job ?",
+job = raw_input()
+print "What is your company name ?",
+company_name = raw_input()
+
+print "Hello, I am %r and I am working as a %r at %r." % (
+	name, job, company_name)
