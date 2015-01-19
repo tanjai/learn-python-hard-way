c1b1134d7951bc7e614b76ea79fa75d4f74c8adc
Tanjai Kongyuen <tanjaik@wotifgroup.com>
2015-01-19 15:53:19 +0700

    Practice Exercise 12

---
+++ Exercise-12-Prompting-People/Ex12.py
@@ -0,0 +1,18 @@
+print "Exercise 12 : Promting People"
+
+age    = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+print "______________"
+print " Study Drills "
+
+name         = raw_input("What is your name? ")
+job          = raw_input("What is your job? ")
+company_name = raw_input("What is your company name? ")
+
+print "Hello, I am %r and I am working as a %r at %r." % (
+	name, job, company_name)
